3a9876951e3fa95545b26aa9844e955a77515c83
iigmir <roc120j@gmail.com>
2019-02-15 18:29:51 +0800

    Add texts

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import "./index.scss";
 function main()
 {
     let app = document.querySelector("#app");
-    let message  = "Hello World";
-    app.innerText = message;
+    let message  = "I am who I am.";
+    let append_title = () =>
+    {
+        let dom = document.createElement("h1");
+        dom.classList.add("foo"); 
+        dom.innerText = "Webpack Success";
+        return dom;
+    };
+    let append_paragraph = message =>
+    {
+        let dom = document.createElement("p");
+        dom.innerText = message;
+        return dom;
+    };
+    let html_render = () =>
+    {
+        let dom_list = [
+            append_title(),
+            append_paragraph("This paragraph use append_paragraph() function!")
+        ];
+        dom_list.forEach( dom => app.appendChild(dom) );
+    };
+    html_render();
 }
 
 main();
